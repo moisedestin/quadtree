de4dcff172039d84c4485e3aabf8577be830cbad
Moïse Destin <moisedestin@gmail.com>
2022-01-15 13:36:15 +0100

    just the particle

--- src/main/java/com/example/quadtreegraphical/HelloApplication.java
+++ src/main/java/com/example/quadtreegraphical/HelloApplication.java
@@ -40,10 +40,6 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
 
 
-
-        Quadtree quadtree = new Quadtree(new Rectangle2D(0,0,800,800), 4);
-
-
         List<Particle> particles = new ArrayList<>();
         for (int i = 0; i < 500; i++) {
             Particle particle = new Particle(randomBetween(100, 600 ), randomBetween(100, 600), scene.getWidth(), scene.getHeight(), Color.AQUA);
@@ -90,74 +86,14 @@ public class HelloApplication extends Application {
             }.start();
 
 
-
-
-
-
         scene.setRoot(root);
 
 
-
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
-
-//        AnchorPane root = new AnchorPane();
-//        Scene scene = new Scene(root, 800, 800);
-//        stage.setScene(scene);
-//
-//
-//
-//
-//
-//        Rectangle2D boundary = new Rectangle2D(100, 100, 600, 600); // with and height are distances from the center( (x,y) which is at (200,200) position, so that's perfectly in the middle) to the edge
-//        Quadtree quadtree = new Quadtree(boundary, 4);
-//
-//        for (int i = 0; i < 1000; i++) {
-//            // the point should be minimum the position of the with and maximum the with * 2
-//            quadtree.insert(new Point2D(randomBetween(100, 600 ), randomBetween(100, 600))); //just to not leave the boitn to the boundary
-////            quadtree.insert(new Point2D(randomBetween(quadtree.boundary.getWidth(), (quadtree.boundary.getWidth() * 2) ), randomBetween(quadtree.boundary.getHeight(), (quadtree.boundary.getHeight() * 2)))); //just to not leave the boitn to the boundary
-//        }
-//
-//        quadtree.show(root);
-//
-//        Rectangle rectangle = new Rectangle(150, 150, 200, 200);
-//        rectangle.setStroke(Color.GREEN);
-//        rectangle.setFill(Color.TRANSPARENT);
-//        root.getChildren().add(rectangle);
-//
-//        List<Point2D> points = quadtree.query(new Rectangle2D(150, 150, 200, 200));
-//        System.out.println(points.size());
-//        for(Point2D point2D: points){
-//            Circle circle = new Circle();
-//            circle.setCenterX(point2D.getX());
-//            circle.setCenterY(point2D.getY());
-//            circle.setStrokeWidth(100);
-//            circle.setFill(Color.AQUA);
-//            circle.setRadius(2);
-//            root.getChildren().add(circle);
-//        }
-//
-//        scene.setRoot(root);
-//
-//        scene.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-//            @Override
-//            public void handle(MouseEvent mouseEvent) {
-//                quadtree.insert(new Point2D(mouseEvent.getX(), mouseEvent.getY())); //just to not leave the boitn to the boundary
-//                quadtree.show(root);
-//            }
-//        });
-//
-//        stage.show();
     }
-
-
-
 
 }
