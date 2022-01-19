f22f6c059727f3cefa2f4e8e153f4e0fa016cccd
Moïse Destin <moisedestin@gmail.com>
2022-01-19 07:25:39 +0100

    f d

--- src/main/java/com/example/quadtreegraphical/Quadtree.java
+++ src/main/java/com/example/quadtreegraphical/Quadtree.java
@@ -14,7 +14,7 @@ public class Quadtree {
         Rectangle2D boundary;
         boolean divided = false;
         int capacity; // capacity
-        ArrayList<Point2D> points;
+        Point2D points;
         Quadtree quadtreenw;
         Quadtree quadtreene;
         Quadtree quadtreesw;
@@ -23,30 +23,28 @@ public class Quadtree {
         Quadtree(Rectangle2D boundary, int n ) {
             this.boundary = boundary;
             this.capacity = n;
-            this.points = new ArrayList<>();
+            this.points = null;
         }
 
         public void insert(Point2D point) {
 
 
             if (  this.boundary.contains(point)){
-                if (this.points.size() < this.capacity) {
-                    this.points.add(point);
+                if (this.points == null) {
+                    this.points = point;
                 } else {
 
                     if (!divided) {
                         this.subdivide();
 
-                        for (Point2D existingpoint :
-                                points) {
-                            if(!point.equals(existingpoint)){
-                                this.quadtreese.insert(existingpoint);
-                                this.quadtreenw.insert(existingpoint);
-                                this.quadtreesw.insert(existingpoint);
-                                this.quadtreene.insert(existingpoint);
+
+                            if(!point.equals(points)){
+                                this.quadtreese.insert(points);
+                                this.quadtreenw.insert(points);
+                                this.quadtreesw.insert(points);
+                                this.quadtreene.insert(points);
                             }
 
-                        }
                     }
 
 
@@ -78,39 +76,16 @@ public class Quadtree {
 
         }
 
-        public List<Point2D> query(Rectangle2D queryRectangle2D){
-            List<Point2D> results = new ArrayList<Point2D>();
-            if(!this.boundary.intersects(queryRectangle2D)){
-            }
-            else{
-                for (Point2D point2D : points) {
-                    if(queryRectangle2D.contains(point2D)){
-                        results.add(point2D);
-                    }
-                }
-
-                if(this.divided){
-                    results.addAll(this.quadtreenw.query(queryRectangle2D));
-                    results.addAll(this.quadtreene.query(queryRectangle2D));
-                    results.addAll(this.quadtreesw.query(queryRectangle2D));
-                    results.addAll(this.quadtreese.query(queryRectangle2D));
-                }
-            }
-
-            return results;
-
-
-        }
 
         public void show(AnchorPane root) {
             Rectangle rectangle = new Rectangle(this.boundary.getMinX(), this.boundary.getMinY(), this.boundary.getWidth(), this.boundary.getHeight());
             rectangle.setStroke(Color.RED);
             rectangle.setFill(Color.TRANSPARENT);
             root.getChildren().add(rectangle);
-            for(Point2D point2D: points){
+            if(points != null){
                 Circle circle = new Circle();
-                circle.setCenterX(point2D.getX());
-                circle.setCenterY(point2D.getY());
+                circle.setCenterX(points.getX());
+                circle.setCenterY(points.getY());
                 circle.setStrokeWidth(100);
                 circle.setFill(Color.BLACK);
                 circle.setRadius(2);
